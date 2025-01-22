414ffa6fed712503bb76bddcc0be29c13ecbe68f
a-zcostumeshire <atozcostumesnam@gmail.com>
2025-01-22 19:07:34 +0200

    Create EditCostumeModal.js

---
+++ src/components/EditCostumeModal.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { CostumeForm } from './CostumeForm';
+
+export const EditCostumeModal = ({ costume, isOpen, onClose, onSave }) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit Costume</DialogTitle>
+        </DialogHeader>
+        <CostumeForm 
+          initialData={costume} 
+          onSuccess={() => {
+            onSave();
+            onClose();
+          }} 
+          isEditing={true}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
