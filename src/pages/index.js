1061cfd8c15cf1f99dcaf347c6c32c594078a236
a-zcostumeshire <atozcostumesnam@gmail.com>
2025-01-22 12:20:04 +0200

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import { CostumeCatalog } from '../components/CostumeCatalog';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <CostumeCatalog />
+    </div>
+  );
+}
