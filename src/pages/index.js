b7ba01598ccb22c15a02248eedcf35e3c911f346
a-zcostumeshire <atozcostumesnam@gmail.com>
2025-01-23 20:52:02 +0200

    Delete src/pages/index.js

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import { CostumeCatalog } from '../components/CostumeCatalog';
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <CostumeCatalog />
-    </div>
-  );
-}
